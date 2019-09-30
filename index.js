ccfc2bde0abfd63122325dc4174ab987ec8f0ba3
İlyas <ashnwor@gmail.com>
2019-10-01 00:51:12 +0300

    general: add location, artist, title

--- index.js
+++ index.js
@@ -3,13 +3,41 @@ const app = express();
 const port = process.env.PORT || 3500;
 
 const songs = [
-  { id: 1, name: "To The Light", artist: "A.CHAL", cover: "cover.jpg"},
-  { id: 2, name: "Echoes", artist: "Thomas Azier"},
-  { id: 3, name: "Rocket Man", artist: "My Morning Jacket" },
-  { id: 4, name: "Space Child", artist: "UFO" },
-  { id: 5, name: "Footsteps", artist: "Pearl Jam" },
-
-
+  {
+    id: 1,
+    name: "To The Light",
+    artist: "A.CHAL",
+    cover: "cover.jpg",
+    location: "to-the-light.m4a"
+  },
+  {
+    id: 2,
+    name: "Echoes",
+    artist: "Thomas Azier",
+    cover: "coverEcho.jpg",
+    location: "echoes.m4a"
+  },
+  {
+    id: 3,
+    name: "Rocket Man",
+    artist: "My Morning Jacket",
+    cover: "coverJacket.jpg",
+    location: "rocketman.m4a"
+  },
+  {
+    id: 4,
+    name: "Space Child",
+    artist: "UFO",
+    cover: "coverUFO.jpg",
+    location: "spacechild.m4a"
+  },
+  {
+    id: 5,
+    name: "Footsteps",
+    artist: "Pearl Jam",
+    cover: "coverFootsteps.jpg",
+    location: "footsteps.m4a"
+  }
 ];
 
 app.get("/", (req, res) => {
@@ -23,6 +51,7 @@ app.get("/andelevandens", (req, res) => {
 app.get("/api/music/lenght", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(String(songs.length));
+  console.log("responsed to a lenght request");
 });
 
 app.get("/api/music/:id", (req, res) => {
