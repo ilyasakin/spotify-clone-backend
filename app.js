c3576154ff715e577b240a9e565ebdf22b0edfda
İlyas <ashnwor@gmail.com>
2019-10-03 22:37:04 +0300

    general: fetch songs from database and send to client

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const db = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3500;
+const bodyParser = require('body-parser');
 require("dotenv/config");
 
 const musicRouter = require('./routes/musicReq');
-const music = require("./models/music");
 
 db.connect(process.env.dbAddress, {useNewUrlParser: true}, () => {
   console.log('Connected to database!!!');
 });
+
+app.use(bodyParser.json());
+
 app.use("/assets", express.static(__dirname + "/assets"));
 
 app.use("/api", musicRouter);
--- routes/musicReq.js
+++ routes/musicReq.js
@@ -1,5 +1,6 @@
 const app = require("express");
 const router = app.Router();
+const musicModel = require('../models/music');
 
 const songs = [
     {
@@ -40,23 +41,48 @@ const songs = [
   ];
 
 
-  router.get("/", (req, res) => {
-      res.send("damn");
-  })
+  router.get("/music", async (req, res) => {
+      try {
+          const musics = await musicModel.find();
+          res.json(musics);
+      } catch(err) {
+          res.json( { message: err } );
+      }
+    });
   router.get("/music/lenght", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(String(songs.length));
   });
 
-  router.get("/music/:id", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    const music = songs.find(m => m.id === parseInt(req.params.id));
-    if (!songs) res.status(404).send("Verilen id'de şarkı bulunamadı");
-    res.send(music);
+  router.get("/music/:id", async (req, res) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+    try {
+        const musicRequest = await musicModel.find({ "id": req.params.id });
+        res.json(musicRequest);
+    
+    } catch(err) {
+        res.json( { message: err } );
+    } 
+
+  /* const music = songs.find(m => m.id === parseInt(req.params.id));
+  if (!songs) res.status(404).send("Verilen id'de şarkı bulunamadı");
+   res.send(music); */
   });
   
-  router.post("api/post", (req, res) => {
-    console.log(req.body);
+  router.post("/postTest", async (req, res) => {
+      const music = new musicModel({
+          id: req.body.id,
+          name: req.body.name,
+          artist: req.body.artist,
+          cover: req.body.cover,
+          location: req.body.location
+      });
+      try {
+          const savedMusic = await music.save();
+          res.json(savedMusic);
+      } catch(err){
+          res.json( {message: err} );
+      }
   });
 
   module.exports = router;
