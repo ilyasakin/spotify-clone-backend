f580ed5e6fa484b0d248a793c7f632e60e7d4bf6
Ashnwor <ashnwor@gmail.com>
2020-08-11 04:48:09 +0300

    general: logout and logoutall

--- src/routes/UsersRoute.js
+++ src/routes/UsersRoute.js
@@ -53,4 +53,28 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+  // Log user out of the application
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/users/logoutall', auth, async (req, res) => {
+  // Log user out of all devices
+  try {
+    req.user.tokens.splice(0, req.user.tokens.length);
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 export default router;
